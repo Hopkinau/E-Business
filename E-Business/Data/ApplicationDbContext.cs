09d7ccebfffcd303efb503a89a62f497006f5ab8
PENG HE <hopkinau@gmail.com>
2023-07-31 15:41:00 +1000

    create new database

--- E-Business/Data/ApplicationDbContext.cs
+++ E-Business/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E_Business.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Business.Data
 {
@@ -11,6 +12,19 @@ namespace E_Business.Data
         //ApplicationDbContext是constructor
         {
 
+        }
+        public DbSet <Category> Categories { get; set; }
+        //Dbset是用来查看Models里面的Category文件内的数据库schema并且移至到本地数据库
+        //需要在Package Manager Consoles 首先输入:add-migration AddcategoryTableToDb
+        //再输入：update-database
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Comendy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Love", DisplayOrder = 3 }
+                );
         }
     }
 }
--- E-Business/Models/Category.cs
+++ E-Business/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace E_Business.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Business.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
