ab5b03db960b3a6e58acc25d01c0b7f805bc67b2
PENG HE <hopkinau@gmail.com>
2023-08-14 12:02:10 +1000

    Update Edit & Delete Button,also the asp-route

--- E-Business/Controllers/CategoryController.cs
+++ E-Business/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace E_Business.Controllers
             {
                 ModelState.AddModelError("name","The DisplayOrder cannot same as name");
             }
+           
             if (ModelState.IsValid)
              //if modelstate in the Model.category is valid, the data will save to the database
              //the validation will check the maxlength etch
@@ -49,5 +50,41 @@ namespace E_Business.Controllers
 
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb=_db.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+       
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot same as name");
+            }
+
+            if (ModelState.IsValid)
+            
+            {
+                _db.Categories.Add(obj);
+                
+                _db.SaveChanges();
+                
+                return RedirectToAction("Index");
+                
+            }
+            return View();
+
+
+        }
     }
 }
